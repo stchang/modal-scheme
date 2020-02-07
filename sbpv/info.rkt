53dda8ea101f8aa618bcec9f0573b9d7944f2587
Stephen Chang <stchang@ccs.neu.edu>
2020-02-07 12:22:07 -0500

    fix deps; add travis ci script

--- sbpv/info.rkt
+++ sbpv/info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define collection "sbpv")
-(define deps '("base" "turnstile"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define deps '("base" "turnstile-lib"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib" "rackunit-macrotypes-lib"))
 (define scribblings '(;("scribblings/sbpv.scrbl" ())
                       ))
 (define pkg-desc "Description Here")
